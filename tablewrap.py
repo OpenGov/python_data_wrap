835480a731cd4046244ebce9730a0d7f6e731bed
Matt <pyrce@stanford.edu>
2012-09-12 13:40:25 -0700

    Updated tablewrap with squarifyTable and a minor bug fix

--- tablewrap.py
+++ tablewrap.py
@@ -1,5 +1,28 @@
+import sys
 import collections
 from listwrap import ListIter
+
+'''
+Updates a table so that all rows are the same length by
+filling smaller rows with 'None' objects up to the length
+of the largest row.
+
+@author Matt Seal
+'''
+def squarifyTable(table):
+    maxLength = 0
+    minLength = sys.maxint
+    for row in table:
+        rowLen = len(row)
+        if rowLen > maxLength:
+            maxLength = rowLen
+        if rowLen < minLength:
+            minLength = rowLen
+    if maxLength != minLength:
+        for row in table:
+            rowLen = len(row)
+            if rowLen < maxLength:
+                row.extend([None]*(maxLength-rowLen))
 
 '''
 Generates a Transpose Wrapper of a 2D table. The original
@@ -11,7 +34,7 @@ columns to rows.
 '''
 class TableTranpose(collections.Sequence):
     '''
-    Represents a Row of the transpose which is equivilent
+    Represents a Row of the transpose which is equivalent
     to a Column of the original table.
     '''
     class TableTransposeRow(collections.MutableSequence):
@@ -38,11 +61,14 @@ class TableTranpose(collections.Sequence):
         def __iter__(self):
             return ListIter(self)
     
-    def __init__(self, table):
+    def __init__(self, table, verify=True):
         self._table = table
         self._width = len(table) if table else 0
         self._length = len(table[0]) if table and table[0] else 0
-        #TODO verification options
+        if verify:
+            for row in self._table:
+                if len(row) != self._length:
+                    raise ValueError("Non-rectangular table passed to TableTranpose")
         
     def __len__(self):
         return self._length
