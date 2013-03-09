3242170505e049c7de57eaa0d19f04c429295394
Matt <pyrce@stanford.edu>
2013-03-08 16:46:40 -0800

    Fixed pathing issue with __init__ capture

--- datawrap/__init__.py
+++ datawrap/__init__.py
@@ -1,7 +1,9 @@
 import os
 import glob
 # Hack to get setup tools to correctly include all python files
-__all__ = [module.split('.')[0] for module in glob.glob('*.py') if '__' not in module]
-__all__.extend([os.path.split(module)[0] for module in glob.glob('*/__init__.py')])
+__all__ = [module.split(os.path.sep)[-1].split('.')[0] 
+           for module in glob.glob(os.path.dirname(__file__)+'/*.py') if '__' not in module]
+__all__.extend([module.split(os.path.sep)[-2] 
+                for module in glob.glob(os.path.dirname(__file__)+'/*/__init__.py')])
 del glob
 del os
--- testing/__init__.py
+++ testing/__init__.py
@@ -3,8 +3,10 @@ import glob
 import sys
 
 # Hack to get setup tools to correctly include all python files
-__all__ = [module.split('.')[0] for module in glob.glob('*.py') if '__' not in module]
-__all__.extend([os.path.split(module)[0] for module in glob.glob('*/__init__.py')])
+__all__ = [module.split(os.path.sep)[-1].split('.')[0] 
+           for module in glob.glob(os.path.dirname(__file__)+'/*.py') if '__' not in module]
+__all__.extend([module.split(os.path.sep)[-2] 
+                for module in glob.glob(os.path.dirname(__file__)+'/*/__init__.py')])
 
 # Add parent for datawrap import capabilities
 sys.path.append(os.path.abspath(os.path.join(__file__, '..')))
