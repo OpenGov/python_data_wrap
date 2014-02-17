ff462d6b718a4abca8bcbbc54efddc82f4a43392
MSeal <mseal007@gmail.com>
2014-02-17 13:26:36 -0800

    Changed __init__ setters to use allset.

--- setup.py
+++ setup.py
@@ -14,15 +14,20 @@ if (os.path.isdir(build_dir)):
 if (os.path.isdir(dist_dir)):
     shutil.rmtree(dist_dir)
 
+required = [req.strip() for req in read("requirements.txt").splitlines() if req.strip()]
+
 setup(
-    name = "PyDataWrap",
-    version = "1.2.1",
-    author = "Matthew Seal",
-    author_email = "mseal@opengov.com",
-    description = ("Tools for wrapping data and manipulating it in efficient ways"),
-    install_requires=['redis', 'xlrd'],
+    name="PyDataWrap",
+    version="1.2.1",
+    author="Matthew Seal",
+    author_email="mseal@opengov.com",
+    description="Tools for wrapping data and manipulating it in efficient ways",
+    install_requires=required,
     packages=['datawrap'],
-    test_suite = 'tests',
-    zip_safe = False,
+    dependency_links = [
+        "https://github.com/OpenGov/allset/tarball/v1.0.0#egg=allset-1.0.0"
+    ],
+    test_suite='tests',
+    zip_safe=False,
     long_description=read('README.md'),
 )
