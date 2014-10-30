31b9bf5fc7c305acce14a7f376e1fad1a0f64704
MSeal <mseal007@gmail.com>
2014-10-30 13:28:59 -0700

    Added test placeholder back in for generic file loader.

---
+++ tests/file_load_test.py
@@ -0,0 +1,19 @@
+# This import fixes sys.path issues
+import parentpath
+
+import unittest
+import os
+from os.path import dirname
+from datawrap import fileloader
+
+# TODO implement
+class DataLoadTest(unittest.TestCase):
+    def setUp(self):
+        self.data_dir = os.path.join(dirname(__file__), 'file_load_data')
+
+    def test_not_implemented(self):
+        pass
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
